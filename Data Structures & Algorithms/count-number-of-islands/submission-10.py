bed39392ff9d94d228753de8c715f6120fd20933
Pravat <pravat.bhusal@gmail.com>
2026-04-23 01:20:14 -0400

    Add: count-number-of-islands - submission-10

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-10.py
@@ -0,0 +1,50 @@
+"""
+Solution is very simple.
+Mark visited nodes from "1" to "0" to not visit them again
+"""
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+
+        def bfs(row, col):
+            queue = deque()
+            queue.append((row, col))
+            
+            while queue:
+                cell = queue.popleft()
+
+                # mark as visited
+                row, col = cell
+
+                # add right cell to the queue
+                r_col = col + 1
+                if r_col < len(grid[0]) and grid[row][r_col] == "1":
+                    grid[row][r_col] = "0"
+                    queue.append((row, r_col))
+
+                # add left cell to the queue
+                l_col = col - 1
+                if l_col >= 0 and grid[row][l_col] == "1":
+                    grid[row][l_col] = "0"
+                    queue.append((row, l_col))
+
+                # add down cell to the queue
+                d_row = row + 1
+                if d_row < len(grid) and grid[d_row][col] == "1":
+                    grid[d_row][col] = "0"
+                    queue.append((d_row, col))
+
+                # add up cell to the queue
+                u_row = row - 1
+                if u_row >= 0 and grid[u_row][col] == "1":
+                    grid[u_row][col] = "0"
+                    queue.append((u_row, col))
+
+        islands = 0
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == "1":
+                    # count this island
+                    islands += 1
+                    bfs(row, col)
+        
+        return islands
