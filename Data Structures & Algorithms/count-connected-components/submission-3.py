59aa8fbf99966d0dbca5638a14fc9f7fec423e06
Pravat <pravat.bhusal@gmail.com>
2026-05-01 11:23:06 -0500

    Add: count-connected-components - submission-3

---
+++ Data Structures & Algorithms/count-connected-components/submission-3.py
@@ -0,0 +1,41 @@
+"""
+Create an adjancency list from the edges.
+
+Then run a loop to BFS on each node from the list. In the BFS, we need to keep track of
+the visited nodes. For any node in that visited set, it's within a single connected component.
+
+On each iteration, do not re-visit nodes in the visited set. When a node is not in the
+visited set, that's a new component.
+"""
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        adj_list = defaultdict(list)
+        for a, b in edges:
+            # undirected graph, add both nodes from the edge
+            adj_list[a].append(b)
+            adj_list[b].append(a)
+        
+        # BFS for a single component
+        visited = set()
+        def bfs(start):
+            queue = deque()
+            queue.append(start)
+
+            while queue:
+                node = queue.popleft()
+                visited.add(node)
+                
+                for neighbor in adj_list[node]:
+                    if neighbor not in visited:
+                        queue.append(neighbor)
+
+        # count each connected component
+        result = 0
+        for node in adj_list.keys():
+            if node not in visited:
+                bfs(node)
+                result += 1
+        
+        # add the remainder of nodes not visited (disconnected nodes)
+        rem = n - len(visited)
+        return result + rem
