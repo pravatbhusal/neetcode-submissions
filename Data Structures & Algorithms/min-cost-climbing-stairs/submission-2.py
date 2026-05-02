7f16874e873e85328a20587bdbd6408ff0f4ce19
Pravat <pravat.bhusal@gmail.com>
2026-05-02 15:13:43 -0500

    Add: min-cost-climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-2.py
@@ -0,0 +1,25 @@
+"""
+Similar problem to "Climbing Stairs", but the twist is we want the min number
+of costs to finish iterating the array.
+
+Greedy cannot solve this, local optimum -> global optimum is not true.
+Ex: [10, 15, 20]
+If we are greedy we start at 10, forced to go to 15 or 20. Total cost is 25.
+Instead, if we started at 15, total cost is 15.
+
+We need to explore all decision paths. This can be modeled using a Decision Tree.
+DP array will store the min cost at each index i:
+dp[i] = cost[i] + min(dp[i - 1], dp[i - 2])
+"""
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        i_1 = cost[1]
+        i_2 = cost[0]
+
+        for i in range(2, len(cost)):
+            # dp[i] = cost[i] + min(dp[i - 1], dp[i - 2])
+            temp = i_1
+            i_1 = cost[i] + min(i_1, i_2)
+            i_2 = temp
+
+        return min(i_1, i_2)
