8cc5c6c5754def3071fb51607ba7a363049bac12
Pravat <pravat.bhusal@gmail.com>
2026-05-06 22:08:17 -0500

    Add: coin-change - submission-19

---
+++ Data Structures & Algorithms/coin-change/submission-19.py
@@ -0,0 +1,43 @@
+"""
+At first, I see the "fewest numnber of coins" and my intuition is greedy solution to always use the largest coin.
+Greedy fails for example: coins = [1, 3, 4], amount = 6
+We would pick 4, then 3 (cannot do), then 1, then 1 = 3 coins needed.
+However, actual solution is 3, 3 = 2 coins needed.
+
+Since greedy fails, we can use DP to solve this.
+Find the most optimal way to coin change create a DP array of size amount, and build up the solution till amount.
+
+Ex: coins = [1, 5, 10], amount = 12
+dp[0] = 0
+dp[1] = 1
+dp[2] = 2
+...
+dp[5] = 1
+dp[6] = 2
+dp[7] = 3
+...
+dp[10] = 1
+dp[11] = 2
+dp[12] = 3
+
+Solution:
+1. If i in coins set, then dp[i] = 1
+2. If i not in coins set, then dp[i] = dp[i - 1] + remainder
+"""
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if not amount:
+            return 0
+
+        dp = [math.inf] * (amount + 1)
+        
+        for amt in range(amount + 1):
+            for c in coins:
+                if c == amt:
+                    # most efficient to use the exact coin that equals amount
+                    dp[amt] = 1
+                if amt - c >= 0:
+                    # compare with smallest sub-problem and use this coin (add +1)
+                    dp[amt] = min(dp[amt], dp[amt - c] + 1)
+        
+        return dp[amount] if dp[amount] is not math.inf else -1
