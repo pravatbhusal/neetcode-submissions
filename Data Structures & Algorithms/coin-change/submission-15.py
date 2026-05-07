68588072b5ef86e06a6badba23df3ab9bd31ff5f
Pravat <pravat.bhusal@gmail.com>
2026-05-06 22:03:54 -0500

    Add: coin-change - submission-15

---
+++ Data Structures & Algorithms/coin-change/submission-15.py
@@ -0,0 +1,41 @@
+"""
+At first, I see the "fewest numnber of coins" and my intuition is greedy solution to always use the largest coin.
+Greedy fails for example: coins = [1, 3, 4], amount = 6
+We would pick 4, then 3 (cannot do), then 1, then 1 = 3 coins needed.
+However, actual solution is 3, 3 = 2 coins needed.
+
+Since greedy fails, we can use DP to solve this.
+Find the most optimal way to coin change create a DP array of size amount, and build up the solution till amount.
+
+Ex: coins = [1, 5, 10], amount = 12
+dp[0] = 0
+dp[1] = 1
+dp[2] = 2
+...
+dp[5] = 1
+dp[6] = 2
+dp[7] = 3
+...
+dp[10] = 1
+dp[11] = 2
+dp[12] = 3
+
+Solution:
+1. If i in coins set, then dp[i] = 1
+2. If i not in coins set, then dp[i] = dp[i - 1] + remainder
+"""
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if not amount:
+            return 0
+
+        dp = [10001] * (amount + 1)
+        
+        for i in range(amount + 1):
+            for c in coins:
+                if c == i:
+                    dp[i] = 1
+                if i - c >= 0:
+                    dp[i] = min(dp[i], dp[i - c] + 1)
+        
+        return -1 if dp[amount] == 10001 else dp[amount]
