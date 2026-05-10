468895c2f9724c8f1a416442e134498d0a478a2d
Pravat <pravat.bhusal@gmail.com>
2026-05-09 19:31:25 -0500

    Add: count-paths - submission-6

---
+++ Data Structures & Algorithms/count-paths/submission-6.py
@@ -0,0 +1,23 @@
+"""
+Top-down recursive DP solution:
+Number of unique possible paths implies brute-force with recursive bactracking.
+Memoize the grid[i][j] path that we already took to prevent re-counting.
+
+Can we think of a bottom-up tabulation DP solution? Solution:
+
+Initialize dp 2D matrix, which'll store the number of unique paths at dp[i][j].
+
+Recurrence function: dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+This function sums the count of paths from the prev row (i - 1) and prev col (j - 1).
+Doing this function on the entire matrix builds up the total count of unique paths.
+"""
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+         dp = [[1] * n for _ in range(m)]
+         
+         # Build-up the number of unique paths from the prev row and col
+         for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+         
+         return dp[m-1][n-1]
