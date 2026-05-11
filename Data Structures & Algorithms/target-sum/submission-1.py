402f7e3f92b52ed5805e1936ded8d09b8061d1e6
Pravat <pravat.bhusal@gmail.com>
2026-05-11 18:51:29 -0500

    Add: target-sum - submission-1

---
+++ Data Structures & Algorithms/target-sum/submission-1.py
@@ -0,0 +1,35 @@
+"""
+0/1 Knapsack Problem (can only use nums[i] once)
+
+Similar problem: "Coin Change II"
+This problem is the exact same as Coin Change II, but with the idea of addition and subtraction and that we can only
+use the number exactly once (0/1 Knapsack problem).
+
+The dp array will be the length of the target and tells us at dp[i] = # of ways to sum to i
+
+Let's solve this using top-down Recursive Backtracking:
+Base case: Does sum == target?
+Then recursively backtrack the two options:
+    1. Add num at i ->  helper(i + 1, total + nums[i])
+    2. Substract num at i -> helper(i + 1, total - nums[i])
+
+Notice that there are two dimensions (2DP), which is i and the current total.
+"""
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        dp = dict()
+
+        def helper(i, total):
+            dim = (i, total)
+            if dim in dp:
+                return dp[dim]
+            if i == len(nums):
+                return 1 if total == target else 0
+
+            add = helper(i + 1, total + nums[i])
+            sub = helper(i + 1, total - nums[i])
+            dp[dim] = add + sub
+            return dp[dim]
+
+        return helper(0, 0)
+            
