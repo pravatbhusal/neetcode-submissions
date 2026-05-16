b372dd36d6759b664dd43460b4bd455b6c443172
Pravat <pravat.bhusal@gmail.com>
2026-05-16 14:12:53 -0500

    Add: jump-game - submission-1

---
+++ Data Structures & Algorithms/jump-game/submission-1.py
@@ -0,0 +1,24 @@
+"""
+We can be greedy and check the indices from i + nums[i] and jump to the index
+with the largest value. This guarantees we're always picking the largest jump
+length at all times.
+
+Similar to Kadane's algorithm, we don't need to have an inner-loop to track the
+"running best" for indices till i + nums[i]. We'll have a single running variable
+that tracks all prior decision optimally.
+
+We keep checking at index i, can we reach it? If we keep going for all i, then it
+can reach the last index of the list.
+"""
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        max_reach = 0
+
+        for i in range(len(nums)):
+            if max_reach < i:
+                # cannot reach at i, return False
+                return False
+            max_reach = max(max_reach, i + nums[i])
+
+        # max_reach >= i held for all i, so every index was reachable
+        return True
