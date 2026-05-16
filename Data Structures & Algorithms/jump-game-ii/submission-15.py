c01ca744c7ac9f081d0e865f4e85c9174af8b9dd
Pravat <pravat.bhusal@gmail.com>
2026-05-16 16:34:16 -0500

    Add: jump-game-ii - submission-15

---
+++ Data Structures & Algorithms/jump-game-ii/submission-15.py
@@ -0,0 +1,28 @@
+"""
+Similar to "Jump Game", but in this problem we're minimizing the number of jumps.
+
+From Jump Game I: Greedily get the max reach at each index i, which lets us pick the
+jump that would skip the most indices. By maximally skipping, we minimize # of jumps.
+
+But we must also keep a sliding window to tell us when we reach the end of our jump.
+We can use a jump end variable to track the end of the window.
+
+Lastly, if at the end of the list our max reach is larger than len(nums), we
+may be off-by-one. To prevent this, we terminate incrementing jumps before
+the end of the list.
+"""
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        max_reach = 0
+        jump_end = 0
+        jumps = 0
+
+        for i in range(len(nums) - 1):
+            max_reach = max(max_reach, i + nums[i])
+            if jump_end == i:
+                # reached the end of this jump
+                jumps += 1
+                # start new jump
+                jump_end = max_reach
+
+        return jumps
