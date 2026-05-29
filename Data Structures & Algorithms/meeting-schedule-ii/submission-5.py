2ade6220f85e139715575dcc381dba7de4f2774b
Pravat <pravat.bhusal@gmail.com>
2026-05-29 17:15:07 -0500

    Add: meeting-schedule-ii - submission-5

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-5.py
@@ -0,0 +1,49 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+"""
+Similar Problem: "Merge Intervals"
+
+In Merge Intervals you combine overlapping intervals and count the merged groups.
+Here it's the opposite — you want to count how many intervals are simultaneously
+active at "max" overlap.
+
+Sort the intervals to put the overlapping intervals adjacent to each other.
+
+There are two cases to find min number of rooms:
+1. No overlap: curInterval has no conflicts and no new room is needed
+2. Overlap: When two intervals overlap with lastInterval, need a new room
+
+Timeline sweep solved using two ptrs: start_ptr and end_ptr.
+1. Create two sorted arrays of the start and end times for each meeting.
+2. One ptr will track the start time, another ptr will track the end time.
+3. When the start time of a new meeting is less than the earliest end time
+seen so far, we need a new room. When a meeting starts after the earliest
+end time, a room has freed up so we advance end_ptr to the next earliest
+end time.
+"""
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        if not intervals:
+            return 0
+
+        start_times = sorted(intv.start for intv in intervals)
+        end_times = sorted(intv.end for intv in intervals)
+
+        rooms = 0
+        end_ptr = 0
+
+        for start_ptr in range(len(start_times)):
+            if start_times[start_ptr] >= end_times[end_ptr]:
+                # no overlap: a room freed up, reuse it
+                end_ptr += 1
+            else:
+                # overlap: new meeting starts before earliest ending, need a new room
+                rooms += 1
+
+        return rooms
