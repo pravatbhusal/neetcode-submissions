3567cafbe6d029477e2594855e58dcb48151b242
Pravat <pravat.bhusal@gmail.com>
2026-05-29 11:30:28 -0500

    Add: meeting-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-1.py
@@ -0,0 +1,41 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+"""
+Similar Problem: "Merge Intervals"
+
+Sort the intervals to put the overlapping intervals adjacent to each other.
+
+This problem is to count the number of non-overlapping intervals. Non-overlapping
+intervals can have their own room. Overlapping intervals must be put in a
+separate room.
+
+There are two cases to find min number of rooms:
+1. No overlap: curInterval has no conflicts and no new room is needed
+2. Overlap: When two intervals overlap with lastInterval, need a new room
+"""
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        if not intervals:
+            return 0
+
+        start_times = sorted(intv.start for intv in intervals)
+        end_times = sorted(intv.end for intv in intervals)
+
+        rooms = 0
+        end_ptr = 0
+
+        for start_ptr in range(len(start_times)):
+            if start_times[start_ptr] < end_times[end_ptr]:
+                # overlap: new meeting starts before earliest ending, need a new room
+                rooms += 1
+            else:
+                # no overlap: a room freed up, reuse it
+                end_ptr += 1
+
+        return rooms
