69f38b9ebe16c60fcfa033a0353a470af01c98f0
Pravat <pravat.bhusal@gmail.com>
2026-05-29 21:17:59 -0500

    Add: rotate-matrix - submission-6

---
+++ Data Structures & Algorithms/rotate-matrix/submission-6.py
@@ -0,0 +1,14 @@
+"""
+Transpose the matrix: flip row to columns and vice versa.
+
+If we flip the rows to columns of the vertical reverse matrix, it's the rotated image matrix.
+"""
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        # Reverse (vertical flip)
+        matrix.reverse()
+
+        # Transpose (swap row and col cell)
+        for row in range(len(matrix)):
+            for col in range(row + 1, len(matrix)):
+                matrix[row][col], matrix[col][row] = matrix[col][row], matrix[row][col]
