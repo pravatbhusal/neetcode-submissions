bfb77bb5e642c78c78f7ee47b4fb69d73d3ee731
Pravat <pravat.bhusal@gmail.com>
2026-05-31 14:09:35 -0500

    Add: pow-x-n - submission-7

---
+++ Data Structures & Algorithms/pow-x-n/submission-7.py
@@ -0,0 +1,39 @@
+"""
+Power of x^n is multiplying x up till n number of times. This is an O(N) solution.
+Example: x = 2, n = 5 -> 2 * 2 * 2 * 2 * 2 * 2 = 64
+
+If you notice, there's repeated work where we multiply 2 * 2 we know the result so we don't need to keep
+multiplying that by the next sequence. We can solve this in O(log(N)) with divide-and-conquer.
+See example 2^6 = 2^3 * 2^3, so if we have the answer to 2^3 we don't need to repeat that work twice.
+
+This idea is called "fast exponentiation" (or exponentiation by squaring). Here's an example of fast exponentation,
+which uses formula "x^n = (x^2)^(n/2)" when n is an even number:
+
+Ex: 2^4 = (2^2)^(4/2) = 4^2 = = 16 <- Notice that we only needed 2^2 for this case
+    2^2 = (2^2)^(2/2) = 4^1 = 4 <- Notice that we only needed 2^1 for this case
+    2^1 = 2
+
+We did not have to calculate 2^3 because fast exponentation skipped that calculation. Fast exponentation skips
+calculating odd powers entirely, which makes this solution O(log(N)).
+
+When n is a negative number, use the same calculation and the result we can simply return: 1 / result.
+"""
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if x == 0:
+            return 0
+        if n == 0:
+            return 1
+
+        def helper(x, n):
+            if n == 1:
+                return x
+            if n % 2 == 0:
+                # handle even (fast exponentation)
+                return helper(x * x, n // 2)
+            else:
+                # handle odd
+                return x * helper(x, n - 1)
+
+        result = helper(x, abs(n))
+        return result if n > 0 else 1 / result
