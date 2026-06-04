864808d3bdd9d2d03566edb52499ef6dfaa0089e
Pravat <pravat.bhusal@gmail.com>
2026-06-03 23:15:33 -0500

    Add: reverse-integer - submission-6

---
+++ Data Structures & Algorithms/reverse-integer/submission-6.py
@@ -0,0 +1,26 @@
+"""
+Extract each bit from the end of the number x using % 10 and dividing by / 10.
+"""
+class Solution:
+
+    def reverse(self, x: int) -> int:
+        original = x
+        MIN_INT = -2 ** 31
+        MAX_INT = (2 ** 31) - 1
+        
+        result = 0
+        while x:
+            # extract last digit
+            digit = abs(x) % 10
+            x = int(x / 10)
+
+            # check int overflow before updating result
+            if result > MAX_INT // 10 or (result == MAX_INT // 10 and digit > MAX_INT % 10):
+                return 0
+            if result < MIN_INT // 10 or (result == MIN_INT // 10 and digit < MIN_INT % 10):
+                return 0
+
+            # add digit to result
+            result = (result * 10) + digit
+        
+        return result if original > 0 else -result
